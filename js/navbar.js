cef1189965056179212b6e4dc50189ec1eb27b17
Agustin <ageus94@gmail.com>
2024-01-25 13:28:41 -0600

    Hero, great advance

--- js/navbar.js
+++ js/navbar.js
@@ -1,15 +1,21 @@
 const navbar = document.querySelector(".navbar")
 const navbar_button = document.querySelector(".navbar-button")
 const navbar_items = document.querySelectorAll(".navbar-item")
+const img_hero = document.querySelector(".hero-container").querySelector("img")
 
 window.addEventListener("resize", (event) => {
-    console.log("resize")
     if (window.innerWidth < 1000) {
+
+        img_hero.src = "/img/hero_small.png"
+
         navbar_items.forEach(e => {
             e.classList.remove("show")
             e.classList.add("hide")
         });
     } else {
+
+        img_hero.src = "/img/hero6.png"
+
         navbar_items.forEach(e => {
             e.classList.remove("hide")
             e.classList.add("show")
@@ -27,9 +33,11 @@ if (window.innerWidth < 1000) {
 navbar_button.addEventListener("click", (event) => {
     navbar_items.forEach(e => {
         if (e.classList.contains("show")) {
+            navbar.classList.remove("nav-wt-bg")
             e.classList.remove("show")
             e.classList.add("hide")
         } else {
+            navbar.classList.add("nav-wt-bg")
             e.classList.remove("hide")
             e.classList.add("show")
         }
@@ -38,7 +46,6 @@ navbar_button.addEventListener("click", (event) => {
 
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
-    console.log(scroll)
     
     // Change background of navbar if scroll is not 0
     if (scroll > 0) {
